b0e969e6782966e31185bf083e95a2f7d9baf893
c4llmeco4ch <cbb692@gmail.com>
2020-03-12 15:41:01 -0700

    Added initial test headers for building request

--- tests/create_request_test.py
+++ tests/create_request_test.py
@@ -1,2 +1,23 @@
 import pytest
 
+class TestBuildRequest:
+    """test different ways to manipulate request string"""
+    
+    def test_valid_dates(self):
+        """provided valid dates"""
+        pass
+
+    def test_invalid_dates(self):
+        """provided invalid dates"""
+        pass
+
+    def test_valid_langs(self):
+        """providing valid langs"""
+        pass
+
+    def test_invalid_langs(self):
+        """providing invalid langs"""
+        pass
+
+    def test_valid_locations(self):
+        """providing valid locations"""
