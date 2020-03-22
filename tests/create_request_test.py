821ab38e67a8f2a999831212e775993611476711
c4llmeco4ch <cbb692@gmail.com>
2020-03-22 15:21:49 -0700

    Added english parse headers

--- tests/create_request_test.py
+++ tests/create_request_test.py
@@ -1,8 +1,9 @@
 import pytest
 
+
 class TestBuildRequest:
     """test different ways to manipulate request string"""
-    
+
     def test_valid_dates(self):
         """provided valid dates"""
         pass
@@ -21,3 +22,32 @@ class TestBuildRequest:
 
     def test_valid_locations(self):
         """providing valid locations"""
+        pass
+
+    # Other tests to come
+
+
+class TestEnglishParse:
+    """test reading in english text and parsing for expected letters"""
+
+    def test_normal_string(self):
+        """providing regular sentence"""
+        pass
+
+    def test_empty_string(self):
+        """providing empty string"""
+        pass
+
+    def test_mixed_case(self):
+        """providing string with mixed cases"""
+        pass
+
+    def test_all_caps(self):
+        """providing string with all caps"""
+        pass
+
+    def test_non_english_chars(self):
+        """when language is set to english, non-en characters aren't read"""
+        pass
+
+
