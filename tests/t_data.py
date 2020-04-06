60f027bb781660297405172e3885de20fd26d158
c4llmeco4ch <cbb692@gmail.com>
2020-04-05 17:26:22 -0700

    removed 'q=' from date tests

--- tests/t_data.py
+++ tests/t_data.py
@@ -1,9 +1,9 @@
 from datetime import date
-dates = [('1:1:2020', r'q=since%3A2020%2D1%2D1'),
+dates = [('1:1:2020', r'since%3A2020%2D1%2D1'),
          ('-1:4:2020', False),
          ('2:30:2019', False),
          ('%d:%d:%d' % ((t:=date.today()).month, t.day, t.year),
-              'q=since%3A{year}%2D{month}%2D{day}'.format(
+                'since%3A{year}%2D{month}%2D{day}'.format(
                 year=t.year,
                 month=t.month,
                 day=t.day),
